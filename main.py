6a103cea72f35d5b4c915828eb1d774217537e38
CompuGenius Programs <contact@cgprograms.com>
2021-04-05 14:17:21 -0400

    Added simon_says() and made the program interactable, so you use it on the fly, with no prior configuration.

--- main.py
+++ main.py
@@ -1,9 +1,54 @@
-option = input("Generate template [0] or start solver [1] ? ")
+import solver
 
-if option == '0':
-    import template_generator
-    template_generator.main()
+_split = lambda x: x.split(', ') if ' ' in x else x.split(',')
+ordinal = lambda n: "%d%s" % (n, "tsnrhtdd"[(n // 10 % 10 != 1) * (n % 10 < 4) * n % 10::4])
 
-elif option == '1':
-    import starting_template
-    starting_template.main()
+indicators = {
+    'SND': False,
+    'CLR': False,
+    'CAR': False,
+    'IND': False,
+    'FRQ': False,
+    'SIG': False,
+    'NSA': False,
+    'MSA': False,
+    'TRN': False,
+    'BOB': False,
+    'FRK': False
+}
+
+serial = input("Serial number? ")
+battery_count = int(input("Battery count? "))
+
+lit_indicators = input("Enter lit indicators separated by a comma: ").upper()
+lit_indicators = _split(lit_indicators)
+
+for indicator in lit_indicators:
+    indicators[indicator] = True
+
+while True:
+    module_name = input("Module name? ").lower()
+
+    if module_name == 'done':
+        break
+
+    elif module_name == "wires":
+        wires_list = input("Enter the wires' colors separated by a comma: ").lower()
+        wires_list = _split(wires_list)
+
+        print("Cut the \"%s Wire\"" % ordinal(solver.wires(wires_list, serial)))
+
+    elif module_name == "button":
+        color = input("Button's color? ")
+        text = input("Button's text? ")
+
+        print("%s" % solver.button({"color": color, "text": text}, battery_count, indicators).upper())
+
+    elif module_name == "simon says":
+        solver.simon_says(serial)
+
+    elif module_name == "passwords":
+        letter_groups = input("Enter the letter groups separated by a comma: ").lower()
+        letter_groups = _split(letter_groups)
+
+        print("Enter in \"%s\"" % solver.password(letter_groups))
--- solver.py
+++ solver.py
@@ -1,13 +1,13 @@
-import starting_template
+import re
 
 
 def last_of_instance(list_of_elements, instance):
     return next(i for i in reversed(range(len(list_of_elements))) if list_of_elements[i] == instance) + 1
 
 
-def last_digit_odd():
+def last_digit_odd(serial):
     possible_digits = []
-    for letter in starting_template.serial:
+    for letter in serial:
         if letter.isnumeric():
             possible_digits.append(int(letter))
     if len(possible_digits) >= 1:
@@ -16,76 +16,182 @@ def last_digit_odd():
     return False
 
 
-def wires(wires_group):
-    wires_list = []
+def wires(wires_list, serial):
+    if len(wires_list) == 3:
+        if "red" not in wires_list:
+            return 2
+        elif wires_list[-1] == "white":
+            return len(wires_list)
+        elif wires_list.count("blue") > 1:
+            return last_of_instance(wires_list, 'blue')
+        else:
+            return len(wires_list)
 
-    for wires_group in wires_group:
-        if len(wires_group) == 3:
-            if "red" not in wires_group:
-                wires_list.append(2)
-            elif wires_group[-1] == "white":
-                wires_list.append(len(wires_group))
-            elif wires_group.count("blue") > 1:
-                wires_list.append(last_of_instance(wires_group, 'blue'))
+    elif len(wires_list) == 4:
+        if wires_list.count("red") > 1 and last_digit_odd(serial):
+            return last_of_instance(wires_list, 'red')
+        elif wires_list[-1] == 'yellow' and wires_list.count("red") == 0:
+            return 1
+        elif wires_list.count("blue") == 1:
+            return 1
+        elif wires_list.count("yellow") > 1:
+            return len(wires_list)
+        else:
+            return 2
+
+    elif len(wires_list) == 5:
+        if wires_list[-1] == "black" and last_digit_odd(serial):
+            return 4
+        elif wires_list.count("red") == 1 and wires_list.count("yellow") > 1:
+            return 1
+        elif wires_list.count("black") == 0:
+            return 2
+        else:
+            return 1
+
+    elif len(wires_list) == 6:
+        if wires_list.count("yellow") == 0 and last_digit_odd(serial):
+            return 3
+        elif wires_list.count("yellow") == 1 and wires_list.count("white") > 1:
+            return 4
+        elif wires_list.count("red") == 0:
+            return len(wires_list)
+        else:
+            return 4
+
+
+def button(press_button, battery_count, indicators):
+    color = press_button["color"]
+    text = press_button["text"]
+
+    if color == "blue" and text == "abort":
+        return releasing_held_button()
+    elif battery_count > 1 and text == "detonate":
+        return "press and immediately release button"
+    elif color == "white" and indicators['CAR']:
+        return releasing_held_button()
+    elif battery_count > 2 and indicators['FRK']:
+        return "press and immediately release button"
+    elif color == "yellow":
+        return releasing_held_button()
+    elif color == "red" and text == "hold":
+        return "press and immediately release button"
+    else:
+        return releasing_held_button()
+
+
+def releasing_held_button():
+    color = input("What is the color of the strip after holding the button? ")
+
+    if color == "blue":
+        return "release when the countdown timer has a \"4\" in any position"
+    elif color == "white":
+        return "release when the countdown timer has a \"1\" in any position"
+    elif color == "yellow":
+        return "release when the countdown timer has a \"5\" in any position"
+    else:
+        return "release when the countdown timer has a \"1\" in any position"
+
+
+def simon_says(serial):
+    vowels = re.compile("[AEIOUaeiou]")
+
+    inputted_colors = []
+
+    while True:
+        new_color = input("Newest flashing color? ")
+
+        if new_color == 'done':
+            break
+
+        inputted_colors.append(new_color)
+
+        strike_count = int(input("Strike count? "))
+
+        output_colors = []
+
+        for color in inputted_colors:
+            if bool(vowels.search(serial)):
+                if strike_count == 0:
+                    if color == "red":
+                        output_colors.append("blue")
+                    elif color == "blue":
+                        output_colors.append("red")
+                    elif color == "green":
+                        output_colors.append("yellow")
+                    elif color == "yellow":
+                        output_colors.append("green")
+
+                elif strike_count == 1:
+                    if color == "red":
+                        output_colors.append("yellow")
+                    elif color == "blue":
+                        output_colors.append("green")
+                    elif color == "green":
+                        output_colors.append("blue")
+                    elif color == "yellow":
+                        output_colors.append("red")
+
+                elif strike_count == 2:
+                    if color == "red":
+                        output_colors.append("green")
+                    elif color == "blue":
+                        output_colors.append("red")
+                    elif color == "green":
+                        output_colors.append("yellow")
+                    elif color == "yellow":
+                        output_colors.append("blue")
+
             else:
-                wires_list.append(len(wires_group))
+                if strike_count == 0:
+                    if color == "red":
+                        output_colors.append("blue")
+                    elif color == "blue":
+                        output_colors.append("yellow")
+                    elif color == "green":
+                        output_colors.append("green")
+                    elif color == "yellow":
+                        output_colors.append("red")
 
-        elif len(wires_group) == 4:
-            if wires_group.count("red") > 1 and last_digit_odd():
-                wires_list.append(last_of_instance(wires_group, 'red'))
-            elif wires_group[-1] == 'yellow' and wires_group.count("red") == 0:
-                wires_list.append(1)
-            elif wires_group.count("blue") == 1:
-                wires_list.append(1)
-            elif wires_group.count("yellow") > 1:
-                wires_list.append(len(wires_group))
-            else:
-                wires_list.append(2)
+                elif strike_count == 1:
+                    if color == "red":
+                        output_colors.append("red")
+                    elif color == "blue":
+                        output_colors.append("blue")
+                    elif color == "green":
+                        output_colors.append("yellow")
+                    elif color == "yellow":
+                        output_colors.append("green")
 
-        elif len(wires_group) == 5:
-            if wires_group[-1] == "black" and last_digit_odd():
-                wires_list.append(4)
-            elif wires_group.count("red") == 1 and wires_group.count("yellow") > 1:
-                wires_list.append(1)
-            elif wires_group.count("black") == 0:
-                wires_list.append(2)
-            else:
-                wires_list.append(1)
+                elif strike_count == 2:
+                    if color == "red":
+                        output_colors.append("yellow")
+                    elif color == "blue":
+                        output_colors.append("green")
+                    elif color == "green":
+                        output_colors.append("blue")
+                    elif color == "yellow":
+                        output_colors.append("red")
 
-        elif len(wires_group) == 6:
-            if wires_group.count("yellow") == 0 and last_digit_odd():
-                wires_list.append(3)
-            elif wires_group.count("yellow") == 1 and wires_group.count("white") > 1:
-                wires_list.append(4)
-            elif wires_group.count("red") == 0:
-                wires_list.append(len(wires_group))
-            else:
-                wires_list.append(4)
-
-    return wires_list
+        print("Press the following colors in order: \"%s\"" % ' - '.join(output_colors).upper())
 
 
-def passwords(letter_groups):
+def password(groups):
     words = ['about', 'after', 'again', 'below', 'could', 'every', 'first', 'found', 'great', 'house', 'large', 'learn',
              'never', 'other', 'place', 'plant', 'point', 'right', 'small', 'sound', 'spell', 'still', 'study', 'their',
              'there', 'these', 'thing', 'think', 'three', 'water', 'where', 'which', 'world', 'would', 'write']
 
-    passwords_list = []
+    possible_words = []
+    impossible_words = []
 
-    for groups in letter_groups:
-        possible_words = []
-        impossible_words = []
+    for letter_group in groups:
+        for word in words:
+            if word[groups.index(letter_group)] in letter_group:
+                if word not in possible_words and word not in impossible_words:
+                    possible_words.append(word)
+            else:
+                if word in possible_words:
+                    possible_words.remove(word)
+                impossible_words.append(word)
 
-        for letter_group in groups:
-            for word in words:
-                if word[groups.index(letter_group)] in letter_group:
-                    if word not in possible_words and word not in impossible_words:
-                        possible_words.append(word)
-                else:
-                    if word in possible_words:
-                        possible_words.remove(word)
-                    impossible_words.append(word)
-
-        passwords_list.append(possible_words[0])
-
-    return passwords_list
+    return possible_words[0]
